3b7d6602da26bb876ed5277f4c8be191592c66f2
g0tmi1k <have.you.g0tmi1k@gmail.com>
2015-09-13 17:06:04 +0100

    File Upload - Checks folder permission

--- vulnerabilities/upload/index.php
+++ vulnerabilities/upload/index.php
@@ -8,6 +8,8 @@ dvwaPageStartup( array( 'authenticated', 'phpids' ) );
 $page = dvwaPageNewGrab();
 $page[ 'title' ]  .= $page[ 'title_separator' ].'Vulnerability: File Upload';
 $page[ 'page_id' ] = 'upload';
+$page[ 'help_button' ]   = 'upload';
+$page[ 'source_button' ] = 'upload';
 
 dvwaDatabaseConnect();
 
@@ -29,30 +31,39 @@ switch( $_COOKIE[ 'security' ] ) {
 
 require_once DVWA_WEB_PAGE_TO_ROOT."vulnerabilities/upload/source/{$vulnerabilityFile}";
 
-$page[ 'help_button' ]   = 'upload';
-$page[ 'source_button' ] = 'upload';
-
 $page[ 'body' ] .= "
 <div class=\"body_padded\">
 	<h1>Vulnerability: File Upload</h1>
-	<div class=\"vulnerable_code_area\">
+
+	<div class=\"vulnerable_code_area\">";
+if( is_writable( realpath( dirname( dirname( getcwd() ) ) )."/hackable/uploads/" ) ) {
+	$page[ 'body' ] .= "
 		<form enctype=\"multipart/form-data\" action=\"#\" method=\"POST\" />
 			<input type=\"hidden\" name=\"MAX_FILE_SIZE\" value=\"100000\" />
-			Choose an image to upload:<br />
+			Choose an image to upload:<br /><br />
 			<input name=\"uploaded\" type=\"file\" /><br />
+			<br />
 			<input type=\"submit\" name=\"Upload\" value=\"Upload\" />
 		</form>
 		{$html}
 	</div>
 
-	<h2>More info</h2>
+	<h2>More Information</h2>
 	<ul>
 		<li>".dvwaExternalLinkUrlGet( 'https://www.owasp.org/index.php/Unrestricted_File_Upload' )."</li>
 		<li>".dvwaExternalLinkUrlGet( 'https://blogs.securiteam.com/index.php/archives/1268' )."</li>
 		<li>".dvwaExternalLinkUrlGet( 'https://www.acunetix.com/websitesecurity/upload-forms-threat/' )."</li>
 	</ul>
-</div>
 ";
+}
+else {
+	$page[ 'body' ] .= "
+	The upload folder (<em>".realpath( dirname( dirname( getcwd() ) ) )."/hackable/uploads/"."</em>) cannot be written to by the web server.<br />
+	Please alter the folder permissions before continuing.
+";
+}
+$page[ 'body' ] .= "</div>";
+
 
 dvwaHtmlEcho( $page );
 
--- vulnerabilities/upload/source/high.php
+++ vulnerabilities/upload/source/high.php
@@ -1,30 +1,25 @@
 <?php
 
-if(isset( $_POST[ 'Upload' ] )) {
-	$target_path = DVWA_WEB_PAGE_TO_ROOT."hackable/uploads/";
-	$target_path = $target_path . basename($_FILES[ 'uploaded' ][ 'name' ]);
+if( isset( $_POST[ 'Upload' ] ) ) {
+	$target_path   = DVWA_WEB_PAGE_TO_ROOT."hackable/uploads/";
+	$target_path   = $target_path . basename( $_FILES[ 'uploaded' ][ 'name' ] );
 	$uploaded_name = $_FILES[ 'uploaded' ][ 'name' ];
 	$uploaded_ext  = substr( $uploaded_name, strrpos($uploaded_name, '.' ) + 1);
 	$uploaded_size = $_FILES[ 'uploaded' ][ 'size' ];
 
-	if(($uploaded_ext == "jpg" || $uploaded_ext == "JPG" || $uploaded_ext == "jpeg" || $uploaded_ext == "JPEG") && ($uploaded_size < 100000)) {
-
+	$html .= '<pre>';
+	if( ( strtolower($uploaded_ext) == "jpg" || strtolower($uploaded_ext) == "jpeg" || strtolower($uploaded_ext) == "png" ) && ( $uploaded_size < 100000 ) ) {
 		if(!move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path )) {
-			$html .= '<pre>';
 			$html .= 'Your image was not uploaded.';
-			$html .= '</pre>';
 		}
 		else {
-			$html .= '<pre>';
 			$html .= $target_path . ' succesfully uploaded!';
-			$html .= '</pre>';
 		}
 	}
 	else {
-		$html .= '<pre>';
-		$html .= 'Your image was not uploaded.';
-		$html .= '</pre>';
+		$html .= 'Your image was not uploaded. We only accept JPEG or PNG images.';
 	}
+	$html .= '</pre>';
 }
 
 ?>
