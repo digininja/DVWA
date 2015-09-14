04b3e36deb30ade46827c3341376c906c64aa6c9
g0tmi1k <have.you.g0tmi1k@gmail.com>
2015-09-14 11:55:40 +0100

    Another redirect to setup.php when tables are missing

--- login.php
+++ login.php
@@ -18,8 +18,18 @@ if( isset( $_POST[ 'Login' ] ) ) {
 	$pass = mysql_real_escape_string( $pass );
 	$pass = md5( $pass );
 
+	$query = ("SELECT table_schema, table_name, create_time
+				FROM information_schema.tables
+				WHERE table_schema='{$_DVWA['db_database']}' AND table_name='users'
+				LIMIT 1");
+	$result = @mysql_query( $query );
+	if( mysql_num_rows( $result ) != 1 ) {
+		dvwaMessagePush( "First time.<br />Need to run 'setup.php'." );
+		dvwaRedirect( 'setup.php' );
+	}
+
 	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
-	$result = @mysql_query( $query ) or die( '<pre>' . mysql_error() . '</pre>' );
+	$result = @mysql_query( $query ) or die( '<pre>' . mysql_error() . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 	if( $result && mysql_num_rows( $result ) == 1 ) {    // Login Successful...
 		dvwaMessagePush( "You have logged in as '".$user."'" );
 		dvwaLogin( $user );
