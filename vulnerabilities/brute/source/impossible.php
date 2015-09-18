188cf5406c8e5ef58da356b209b2c30eff1130ce
g0tmi1k <have.you.g0tmi1k@gmail.com>
2015-09-18 10:19:40 +0100

    Fix 'impossible' brute force & file upload (they were possible!)
    + MySQL - could be done better by either:
      - Using cron jobs to clean up
      - MySQL events/triggers
      - IP ban?
      - Add captcha on first failed attempt.
    
    + MySQL - its done now its done to stop user enumeration.
    
    + File Upload - wasn't really vulnerable before, could upload a raw PHP but as a image.
      - Just now now you have to merge them together (and still link them via LFI)

--- vulnerabilities/brute/source/impossible.php
+++ vulnerabilities/brute/source/impossible.php
@@ -2,7 +2,7 @@
 
 if( isset( $_GET[ 'Login' ] ) ) {
 	// Anti-CSRF
-	checkTokens( $_POST[ 'token' ] , "index.php");
+	checkTokens( $_REQUEST[ 'user_token' ], 'index.php' );
 
 	// Sanitise username input
 	$user = $_GET[ 'username' ];
@@ -15,25 +15,54 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = mysql_real_escape_string( $pass );
 	$pass = md5( $pass );
 
-	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
+	$total_failed_login = '3';
+	$lockout_time       = '15';
+	$account_locked     = false;
+
+	$query  = "UPDATE `users` SET `last_login` = now() WHERE user='$user' LIMIT 1;";
+	$result = @mysql_query( $query );
+
+	$query  = "SELECT failed_login,last_login FROM `users` WHERE user='$user' LIMIT 1;";
 	$result = mysql_query( $query ) or die( '<pre>' . mysql_error() . '</pre>' );
 
-	if( $result && mysql_num_rows( $result ) == 1 ) {
+	if( $result && mysql_num_rows( $result ) == 1 && mysql_result( $result, 0, "failed_login" ) >= $total_failed_login ) {
+		// User locked out.  Note, this method will allow for user enumeration!
+		// $html .= "<pre><br />This account has been locked due to too many incorrect logins.</pre>";
+
+		$last_login    = strtotime( mysql_result( $result, 0, "last_login" ) );
+		$timeout_login = strtotime( "{$last_login} +{$lockout_time} minutes" );
+		$timenow       = strtotime( $last_login );
+
+		if( $timeout_login < $timenow )
+			$account_locked = true;
+		else
+			$account_locked = false;
+	}
+
+	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass' LIMIT 1;";
+	$result = mysql_query( $query ) or die( '<pre>' . mysql_error() . '</pre>' );
+
+	if( $result && mysql_num_rows( $result ) == 1 && $account_locked == false) {
 		// Get users details
-		$i = 0; // Bug fix.
-		$avatar = mysql_result( $result, $i, "avatar" );
+		$avatar = mysql_result( $result, 0, "avatar" );
 
 		// Login Successful
 		$html .= "<p>Welcome to the password protected area {$user}</p>";
 		$html .= '<img src="' . $avatar . '" />';
+
+		// Reset bad login count
+		$insert = "UPDATE `users` SET `failed_login` = '0' WHERE user='$user' LIMIT 1;";
+		$result = @mysql_query( $insert );
 	}
 	else {
 		// Login failed
-		sleep(3);
-		$html .= "<pre><br />Username and/or password incorrect.</pre>";
-	}
+		sleep(rand( 2, 4 ) );
 
-	mysql_close();
+		$html .= "<pre><br />Username and/or password incorrect.<br /><br/>Alternative, the account has been locked because of too many failed logins.<br />If this is the case, please try again in {$lockout_time} minutes.</pre>";
+
+		$query  = "UPDATE `users` SET `failed_login` = failed_login + 1 WHERE user='$user' LIMIT 1;";
+		$result = @mysql_query( $query );
+	}
 }
 
 ?>
--- vulnerabilities/upload/source/impossible.php
+++ vulnerabilities/upload/source/impossible.php
@@ -2,16 +2,23 @@
 
 if( isset( $_POST[ 'Upload' ] ) ) {
 	// Anti-CSRF
-	checkTokens( $_POST[ 'token' ] , "index.php");
+	checkTokens( $_REQUEST[ 'user_token' ], 'index.php' );
 
-	$target_path   = DVWA_WEB_PAGE_TO_ROOT."hackable/uploads/";
-	$target_path   = $target_path . basename( $_FILES[ 'uploaded' ][ 'name' ] );
 	$uploaded_name = $_FILES[ 'uploaded' ][ 'name' ];
 	$uploaded_ext  = substr( $uploaded_name, strrpos( $uploaded_name, '.' ) + 1);
 	$uploaded_size = $_FILES[ 'uploaded' ][ 'size' ];
+	$uploaded_type = $_FILES[ 'uploaded' ][ 'type' ];
+	$uploaded_tmp  = $_FILES[ 'uploaded' ][ 'tmp_name' ];
+	$target_path   = DVWA_WEB_PAGE_TO_ROOT."hackable/uploads/";
+	//$target_path   = $target_path . basename( $uploaded_name, '.' . $uploaded_ext ) . '-';
+	$target_path   = $target_path . md5( uniqid() . $uploaded_name ) . "." . $uploaded_ext;
 
 	$html .= '<pre>';
-	if( ( strtolower( $uploaded_ext ) == "jpg" || strtolower( $uploaded_ext ) == "jpeg" || strtolower( $uploaded_ext ) == "png" ) && ( $uploaded_size < 100000 ) ) {
+	if( ( strtolower( $uploaded_ext ) == "jpg" || strtolower( $uploaded_ext ) == "jpeg" || strtolower( $uploaded_ext ) == "png" ) &&
+		( $uploaded_size < 100000 ) &&
+		( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) &&
+		getimagesize( $uploaded_tmp ) ) {
+
 		if(!move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path )) {
 			$html .= 'Your image was not uploaded.';
 		}
