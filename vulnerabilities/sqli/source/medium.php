45eb7da35a8f12f43f9fe5903d30cc4c3fdf60c6
Nick Armstrong <nick@nickarmstrong.id.au>
2016-08-24 18:45:08 +1000

    Update SQL Injection pages to use MySQLi

--- vulnerabilities/sqli/source/medium.php
+++ vulnerabilities/sqli/source/medium.php
@@ -10,18 +10,13 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
 	// Get results
-	$num = mysqli_num_rows( $result );
-	$i   = 0;
-	while( $i < $num ) {
+	while( $row = mysqli_fetch_assoc( $result ) ) {
 		// Display values
-		$first = mysql_result( $result, $i, "first_name" );
-		$last  = mysql_result( $result, $i, "last_name" );
+		$first = $row["first_name"];
+		$last  = $row["last_name"];
 
 		// Feedback for end user
 		$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-
-		// Increase loop count
-		$i++;
 	}
 
 	//mysql_close();
