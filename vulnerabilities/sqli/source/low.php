80f682e4afdaea07d9aa4465d74e4061bc20dbfd
Robin Wood <robin@digi.ninja>
2017-04-12 04:30:04 +0100

    tidied up the sqli source

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -6,7 +6,7 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 
 	// Check database
 	$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	$result = mysqli_query($query ) or die( '<pre>' . mysqli_connect_error() . '</pre>' );
 
 	// Get results
 	while( $row = mysqli_fetch_assoc( $result ) ) {
@@ -18,7 +18,7 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 		$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 	}
 
-	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
+	mysqli_close();
 }
 
 ?>
