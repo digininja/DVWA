965c92b63e7caef953a503b1887be75420e4c768
Robin Wood <robin@digi.ninja>
2017-04-17 22:37:22 +0100

    Fixed my mess up of the sqli vuln

--- vulnerabilities/sqli/source/high.php
+++ vulnerabilities/sqli/source/high.php
@@ -6,7 +6,7 @@ if( isset( $_SESSION [ 'id' ] ) ) {
 
 	// Check database
 	$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id' LIMIT 1;";
-	$result = mysqli_query($query ) or die( '<pre>Something went wrong.</pre>' );
+	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>Something went wrong.</pre>' );
 
 	// Get results
 	while( $row = mysqli_fetch_assoc( $result ) ) {
@@ -18,7 +18,7 @@ if( isset( $_SESSION [ 'id' ] ) ) {
 		$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 	}
 
-	mysqli_close();
+	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);		
 }
 
 ?>
--- vulnerabilities/sqli/source/medium.php
+++ vulnerabilities/sqli/source/medium.php
@@ -3,11 +3,11 @@
 if( isset( $_POST[ 'Submit' ] ) ) {
 	// Get input
 	$id = $_POST[ 'id' ];
-	$id = mysqli_real_escape_string($id);
+	$id = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $id ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
 
 	// Check database
 	$query  = "SELECT first_name, last_name FROM users WHERE user_id = $id;";
-	$result = mysqli_query($query ) or die( '<pre>' . mysqli_connect_error() . '</pre>' );
+	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . mysqli_connect_error() . '</pre>' );
 
 	// Get results
 	while( $row = mysqli_fetch_assoc( $result ) ) {
@@ -19,7 +19,13 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 		$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 	}
 
-	//mysql_close();
 }
 
+// This is used later on in the index.php page
+// Setting it here so we can close the database connection in here like in the rest of the source scripts
+$query  = "SELECT COUNT(*) FROM users;";
+$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+$number_of_rows = mysqli_fetch_row( $result )[0];
+
+mysqli_close($GLOBALS["___mysqli_ston"]);
 ?>
