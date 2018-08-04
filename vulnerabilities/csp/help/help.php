0bc4e6cd95371ef62d581c0fc76190bb87d13e40
Robin Wood <robin@digi.ninja>
2018-08-04 19:20:53 +0100

    csp bypass start

---
+++ vulnerabilities/csp/help/help.php
@@ -0,0 +1,58 @@
+<div class="body_padded">
+	<h1>Help - Content Security Policy (CSP) Bypass</h1>
+
+	<div id="code">
+	<table width='100%' bgcolor='white' style="border:2px #C0C0C0 solid">
+	<tr>
+	<td><div id="code">
+		<h3>About</h3>
+		<p>Blah about CSP.</p>
+
+		<p></p>
+		<p></p>
+		<p></p>
+
+		<br /><hr /><br />
+
+		<h3>Objective</h3>
+		<p>Bypass Content Security Policy (CSP) and execute JavaScript in the page.</p>
+
+		<br /><hr /><br />
+
+		<h3>Low Level</h3>
+		<p>This allows for direct input into one of <u>many PHP functions</u> that will execute commands on the OS. It is possible to escape out of the designed command and
+			executed unintentional actions.</p>
+		<p>This can be done by adding on to the request, "once the command has executed successfully, run this command".
+		<pre>Spoiler: <span class="spoiler">To add a command "&&"</span>. Example: <span class="spoiler">127.0.0.1 && dir</span>.</pre>
+
+		<br />
+
+		<h3>Medium Level</h3>
+		<p>The developer has read up on some of the issues with command injection, and placed in various pattern patching to filter the input. However, this isn't enough.</p>
+		<p>Various other system syntaxes can be used to break out of the desired command.</p>
+		<pre>Spoiler: <span class="spoiler">e.g. background the ping command</span>.</pre>
+
+		<br />
+
+		<h3>High Level</h3>
+		<p>In the high level, the developer goes back to the drawing board and puts in even more pattern to match. But even this isn't enough.</p>
+		<p>The developer has either made a slight typo with the filters and believes a certain PHP command will save them from this mistake.</p>
+		<pre>Spoiler: <span class="spoiler"><?php echo dvwaExternalLinkUrlGet( 'https://secure.php.net/manual/en/function.trim.php', 'trim()' ); ?>
+			removes all leading & trailing spaces, right?</span>.</pre>
+
+		<br />
+
+		<h3>Impossible Level</h3>
+		<p>In the impossible level, the challenge has been re-written, only to allow a very stricted input. If this doesn't match and doesn't produce a certain result,
+			it will not be allowed to execute. Rather than "black listing" filtering (allowing any input and removing unwanted), this uses "white listing" (only allow certain values).</p>
+	</div></td>
+	</tr>
+	</table>
+
+	</div>
+
+	<br />
+
+	<p>Reference: <?php echo dvwaExternalLinkUrlGet( 'https://content-security-policy.com/' ); ?></p>
+	<p>Reference: <?php echo dvwaExternalLinkUrlGet( 'https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP' ); ?></p>
+</div>
--- vulnerabilities/csp/index.php
+++ vulnerabilities/csp/index.php
@@ -1,24 +1,71 @@
 <?php
-//CSP only works in modern browsers Chrome 25+, Firefox 23+, Safari 7+
-$headerCSP = "Content-Security-Policy:".
-"connect-src 'self' ;". // XMLHttpRequest (AJAX request), WebSocket or EventSource.
-"default-src 'self';". // Default policy for loading html elements
-"frame-ancestors 'self' ;". //allow parent framing - this one blocks click jacking and ui redress
-"frame-src 'none';". // vaid sources for frames
-"media-src 'self' *.example.com;". // vaid sources for media (audio and video html tags src)
-"object-src 'none'; ". // valid object embed and applet tags src
-"report-uri https://example.com/violationReportForCSP.php;". //A URL that will get raw json data in post that lets you know what was violated and blocked
-"script-src 'self' 'unsafe-inline' https://pastebin.com  example.com code.jquery.com https://ssl.google-analytics.com ;". // allows js from self, jquery and google analytics.  Inline allows inline js
-"style-src 'self' 'unsafe-inline';";// allows css from self and inline allows inline css
-//Sends the Header in the HTTP response to instruct the Browser how it should handle content and what is whitelisted
-//Its up to the browser to follow the policy which each browser has varying support
-header($headerCSP);
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated', 'phpids' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ]   = 'Vulnerability: Content Security Policy (CSP) Bypass' . $page[ 'title_separator' ].$page[ 'title' ];
+$page[ 'page_id' ] = 'csp';
+$page[ 'help_button' ]   = 'csp';
+$page[ 'source_button' ] = 'csp';
+
+dvwaDatabaseConnect();
+
+$vulnerabilityFile = '';
+switch( $_COOKIE[ 'security' ] ) {
+	case 'low':
+		$vulnerabilityFile = 'low.php';
+		break;
+	case 'medium':
+		$vulnerabilityFile = 'medium.php';
+		break;
+	case 'high':
+		$vulnerabilityFile = 'high.php';
+		break;
+	default:
+		$vulnerabilityFile = 'impossible.php';
+		break;
+}
+
+require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/xss_d/source/{$vulnerabilityFile}";
+
+$page[ 'body' ] = <<<EOF
+<div class="body_padded">
+	<h1>Vulnerability: Content Security Policy (CSP) Bypass</h1>
+
+	<div class="vulnerable_code_area">
+ 
+ 		<p>Please choose a language:</p>
+
+		<form name="XSS" method="GET">
+			<select name="default">
+				<script>
+					if (document.location.href.indexOf("default=") >= 0) {
+						var lang = document.location.href.substring(document.location.href.indexOf("default=")+8);
+						document.write("<option value='' disabled='disabled'>----</option>");
+					}
+					    
+					document.write("<option value='English'>English</option>");
+					document.write("<option value='French'>French</option>");
+					document.write("<option value='Spanish'>Spanish</option>");
+					document.write("<option value='German'>German</option>");
+				</script>
+			</select>
+			<input type="submit" value="Select" />
+		</form>
+	</div>
+EOF;
+
+$page[ 'body' ] .= "
+	<h2>More Information</h2>
+	<ul>
+		<li>" . dvwaExternalLinkUrlGet( 'https://content-security-policy.com/' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP' ) . "</li>
+	</ul>
+</div>\n";
+
+dvwaHtmlEcho( $page );
+
 ?>
-
-<script>
-alert(1);
-</script>
-
-<script src="https://pastebin.com/raw/R570EE00"></script>
-
-Hello
