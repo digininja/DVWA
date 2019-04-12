f9471f36f11a497e9f1a1d70e98aee73ed3ab07e
rastafar11 <49572702+rastafar11@users.noreply.github.com>
2019-04-12 21:27:40 +0000

    Update low.php, patch XSS

--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -5,7 +5,7 @@ header ("X-XSS-Protection: 0");
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	// Feedback for end user
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
+	$html .= '<pre>Hello ' . htmlspecialchars($_GET[ 'name' ], ENT_QUOTES, 'UTF-8') . '</pre>';
 }
 
 ?>
