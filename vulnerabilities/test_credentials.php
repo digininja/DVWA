4b6c09e0e36f201e89a31031a6c3c30d50e373b6
lijantropique <lijantropique@protonmail.com>
2020-05-02 07:02:59 -0400

    tidy up test_credentials.php

--- vulnerabilities/test_credentials.php
+++ vulnerabilities/test_credentials.php
@@ -10,21 +10,15 @@ if( isset( $_POST[ 'Login' ] ) ) {
 
 	$user = $_POST[ 'username' ];
 	$user = stripslashes( $user );
-	$user = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $user ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+	$user = mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $user);
 
 	$pass = $_POST[ 'password' ];
 	$pass = stripslashes( $pass );
-	$pass = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+	$pass = mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $pass);
 	$pass = md5( $pass );
 
-	$query = ("SELECT table_schema, table_name, create_time
-				FROM information_schema.tables
-				WHERE table_schema='{$_DVWA['db_database']}' AND table_name='users'
-				LIMIT 1");
-	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query );
-
 	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
-	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
+	$result = @mysqli_query($GLOBALS["___mysqli_ston"], $query) or die( '<pre>'.  mysqli_connect_error() . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 	if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
 		dvwaMessagePush( "Valid Credentials" );
 		dvwaMessagePush( "You have logged in as '{$user}'" );
