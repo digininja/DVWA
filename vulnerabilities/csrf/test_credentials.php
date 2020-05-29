bec64d64b2f3b52d2a21155373e3fab9cd6ee695
lijantropique <lijantropique@protonmail.com>
2020-05-29 12:53:42 -0400

    Move response message CSRF-test credentials

--- vulnerabilities/csrf/test_credentials.php
+++ vulnerabilities/csrf/test_credentials.php
@@ -5,6 +5,7 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( 'authenticated', 'phpids' ) );
 dvwaDatabaseConnect();
+$login_state = "";
 
 if( isset( $_POST[ 'Login' ] ) ) {
 
@@ -20,10 +21,10 @@ if( isset( $_POST[ 'Login' ] ) ) {
 	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
 	$result = @mysqli_query($GLOBALS["___mysqli_ston"], $query) or die( '<pre>'.  mysqli_connect_error() . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 	if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
-		dvwaMessagePush( "<h3 class=\"loginSuccess\">Valid password for '{$user}'</h3>" );
+		$login_state = "<h3 class=\"loginSuccess\">Valid password for '{$user}'</h3>";
 	}else{
 		// Login failed
-		dvwaMessagePush( "<h3 class=\"loginFail\">Wrong password for '{$user}'</h3>" );
+		$login_state = "<h3 class=\"loginFail\">Wrong password for '{$user}'</h3>";
 	}
 
 }
@@ -37,11 +38,11 @@ $page[ 'body' ] .= "
 			<h2>Vulnerabilities/CSRF</h2>
 			<div id=\"code\">
 				<form action=\"" . DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/csrf/test_credentials.php\" method=\"post\">
-					<fieldset><br />
-							<label for=\"user\">Username</label><br /> <input type=\"text\" class=\"loginInput\" size=\"20\" name=\"username\"><br />
-							<label for=\"pass\">Password</label><br /> <input type=\"password\" class=\"loginInput\" AUTOCOMPLETE=\"off\" size=\"20\" name=\"password\"><br />
-							<br />
-							<p class=\"submit\"><input type=\"submit\" value=\"Login\" name=\"Login\"></p>
+					<fieldset>
+						" . $login_state . "
+						<label for=\"user\">Username</label><br /> <input type=\"text\" class=\"loginInput\" size=\"20\" name=\"username\"><br />
+						<label for=\"pass\">Password</label><br /> <input type=\"password\" class=\"loginInput\" AUTOCOMPLETE=\"off\" size=\"20\" name=\"password\"><br />
+						<p class=\"submit\"><input type=\"submit\" value=\"Login\" name=\"Login\"></p>
 					</fieldset>
 				</form>
 				{$messagesHtml}
