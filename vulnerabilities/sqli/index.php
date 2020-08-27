40be5ab8d849112b585349f1637850f2488af364
Robin Wood <robin@digi.ninja>
2020-08-27 15:35:32 +0100

    more broken links

--- vulnerabilities/sqli/index.php
+++ vulnerabilities/sqli/index.php
@@ -82,12 +82,11 @@ $page[ 'body' ] .= "
 
 	<h2>More Information</h2>
 	<ul>
-		<li>" . dvwaExternalLinkUrlGet( 'http://www.securiteam.com/securityreviews/5DP0N1P76E.html' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://www.securiteam.com/securityreviews/5DP0N1P76E.html' ) . "</li>
 		<li>" . dvwaExternalLinkUrlGet( 'https://en.wikipedia.org/wiki/SQL_injection' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'http://ferruh.mavituna.com/sql-injection-cheatsheet-oku/' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'http://pentestmonkey.net/cheat-sheet/sql-injection/mysql-sql-injection-cheat-sheet' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.owasp.org/index.php/SQL_Injection' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'http://bobby-tables.com/' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://www.netsparker.com/blog/web-security/sql-injection-cheat-sheet/' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/attacks/SQL_Injection' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://bobby-tables.com/' ) . "</li>
 	</ul>
 </div>\n";
 
