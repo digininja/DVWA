1c2c645809b70b2db825552ddb0db1bbe2fa83a8
kevin <>
2020-09-12 13:32:27 +0700

    remove protocol from hastebin, update source comments, update help file

--- vulnerabilities/csp/source/low.php
+++ vulnerabilities/csp/source/low.php
@@ -1,9 +1,10 @@
 <?php
 
-$headerCSP = "Content-Security-Policy: script-src 'self' https://pastebin.com http://hastebin.com example.com code.jquery.com https://ssl.google-analytics.com ;"; // allows js from self, pastebin.com, hastebin.com, jquery and google analytics.
+$headerCSP = "Content-Security-Policy: script-src 'self' https://pastebin.com hastebin.com example.com code.jquery.com https://ssl.google-analytics.com ;"; // allows js from self, pastebin.com, hastebin.com, jquery and google analytics.
 
 header($headerCSP);
 
+# These might work if you can't create your own for some reason
 # https://pastebin.com/raw/R570EE00
 # https://hastebin.com/raw/ohulaquzex
 
@@ -16,7 +17,6 @@ $page[ 'body' ] .= "
 }
 $page[ 'body' ] .= '
 <form name="csp" method="POST">
-	<p><strong>Currently broken due to changes at Pastebin, looking for alternatives.</strong></p>
 	<p>You can include scripts from external sources, examine the Content Security Policy and enter a URL to include here:</p>
 	<input size="50" type="text" name="include" value="" id="include" />
 	<input type="submit" value="Include" />
