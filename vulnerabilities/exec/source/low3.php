8eedefb33e5fb21d060a54380ac4bf43c6e8da99
Martin Bednorz <m.s.bednorz@gmail.com>
2020-12-04 09:27:53 +0100

    Add a new feature

---
+++ vulnerabilities/exec/source/low3.php
@@ -0,0 +1,21 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+	// Get input
+	$target = $_REQUEST[ 'ip' ];
+
+	// Determine OS and execute the ping command.
+	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+		// Windows
+		$cmd = shell_exec( 'ping  ' . $target );
+	}
+	else {
+		// *nix
+		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+	}
+
+	// Feedback for the end user
+	$html .= "<pre>{$cmd}</pre>";
+}
+
+?>
