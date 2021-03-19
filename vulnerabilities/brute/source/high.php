fc06a91ede5c539a54dd71c8c6c57973704b0308
Mohammad AbuDayeh <e@Hotmail.com>
2021-03-20 00:45:09 +0200

    Update high.php
    I have added a condition if there is a session (session_token) is called.
    Because in this case the cookies were removed and a query was sent showing an error:
    
    Notice: Undefined index: session_token in /x/index.php on line 13
    
    Fatal error: Uncaught Error: Call to undefined function dvwaSessionGrab() in /x/function.php:13 Stack trace: #0 /x/function.php(22): dvwaMessagePush('CSRF token is i...') #1 /x/index.php(13): checkToken('9477d94f904941d...', NULL, 'index.php') #2 {main} thrown in /x/function.php on line 13

--- vulnerabilities/brute/source/high.php
+++ vulnerabilities/brute/source/high.php
@@ -2,7 +2,9 @@
 
 if( isset( $_GET[ 'Login' ] ) ) {
 	// Check Anti-CSRF token
-	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+	if (isset($_SESSION[ 'session_token' ])) {
+		checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+	}
 
 	// Sanitise username input
 	$user = $_GET[ 'username' ];
