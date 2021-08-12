7b19264beded23f6e5965551c52821c8c05d428d
Robin Wood <robin@digi.ninja>
2021-08-12 08:57:33 +0100

    stared structuring medium

--- vulnerabilities/sqli/source/medium.php
+++ vulnerabilities/sqli/source/medium.php
@@ -6,19 +6,25 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 
 	$id = mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $id);
 
-	$query  = "SELECT first_name, last_name FROM users WHERE user_id = $id;";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"], $query) or die( '<pre>' . mysqli_error($GLOBALS["___mysqli_ston"]) . '</pre>' );
+	switch ($_DVWA['SQLI_DB']) {
+		case MYSQL:
+			$query  = "SELECT first_name, last_name FROM users WHERE user_id = $id;";
+			$result = mysqli_query($GLOBALS["___mysqli_ston"], $query) or die( '<pre>' . mysqli_error($GLOBALS["___mysqli_ston"]) . '</pre>' );
 
-	// Get results
-	while( $row = mysqli_fetch_assoc( $result ) ) {
-		// Display values
-		$first = $row["first_name"];
-		$last  = $row["last_name"];
+			// Get results
+			while( $row = mysqli_fetch_assoc( $result ) ) {
+				// Display values
+				$first = $row["first_name"];
+				$last  = $row["last_name"];
 
-		// Feedback for end user
-		$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+				// Feedback for end user
+				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+			}
+			break;
+		case SQLITE:
+			global $sqlite_db_connection;
+			break;
 	}
-
 }
 
 // This is used later on in the index.php page
