d1ee14aa30a2f8fbffdba233c588600cdf78c858
Vanye Wadawasina <89488932+Vanye-getcerebral@users.noreply.github.com>
2022-01-24 11:11:57 -0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( 'phpids' ) );
 
-if( !dvwaIsLoggedIn() ) {	// The user shouldn't even be on this page
+if( !dvwaIsLoggedIn() ) {	// The user shouldn't even be on this page just
 	// dvwaMessagePush( "You were not logged in" );
 	dvwaRedirect( 'login.php' );
 }
