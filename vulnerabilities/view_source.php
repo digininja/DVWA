417e5a868745c9a8cab17345e20e602732b5cb31
Robin Wood <robin@digi.ninja>
2022-02-14 21:52:35 +0000

    check parameters exist before accessing

--- vulnerabilities/view_source.php
+++ vulnerabilities/view_source.php
@@ -8,85 +8,89 @@ dvwaPageStartup( array( 'authenticated', 'phpids' ) );
 $page = dvwaPageNewGrab();
 $page[ 'title' ] .= 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
 
-$id       = $_GET[ 'id' ];
-$security = $_GET[ 'security' ];
+if (array_key_exists ("id", $_GET) && array_key_exists ("security", $_GET)) {
+	$id       = $_GET[ 'id' ];
+	$security = $_GET[ 'security' ];
 
 
-switch ($id) {
-	case "fi" :
-		$vuln = 'File Inclusion';
-		break;
-	case "brute" :
-		$vuln = 'Brute Force';
-		break;
-	case "csrf" :
-		$vuln = 'CSRF';
-		break;
-	case "exec" :
-		$vuln = 'Command Injection';
-		break;
-	case "sqli" :
-		$vuln = 'SQL Injection';
-		break;
-	case "sqli_blind" :
-		$vuln = 'SQL Injection (Blind)';
-		break;
-	case "upload" :
-		$vuln = 'File Upload';
-		break;
-	case "xss_r" :
-		$vuln = 'Reflected XSS';
-		break;
-	case "xss_s" :
-		$vuln = 'Stored XSS';
-		break;
-	case "weak_id" :
-		$vuln = 'Weak Session IDs';
-		break;
-	case "javascript" :
-		$vuln = 'JavaScript';
-		break;
-	default:
-		$vuln = "Unknown Vulnerability";
+	switch ($id) {
+		case "fi" :
+			$vuln = 'File Inclusion';
+			break;
+		case "brute" :
+			$vuln = 'Brute Force';
+			break;
+		case "csrf" :
+			$vuln = 'CSRF';
+			break;
+		case "exec" :
+			$vuln = 'Command Injection';
+			break;
+		case "sqli" :
+			$vuln = 'SQL Injection';
+			break;
+		case "sqli_blind" :
+			$vuln = 'SQL Injection (Blind)';
+			break;
+		case "upload" :
+			$vuln = 'File Upload';
+			break;
+		case "xss_r" :
+			$vuln = 'Reflected XSS';
+			break;
+		case "xss_s" :
+			$vuln = 'Stored XSS';
+			break;
+		case "weak_id" :
+			$vuln = 'Weak Session IDs';
+			break;
+		case "javascript" :
+			$vuln = 'JavaScript';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.php" );
+	$source = str_replace( array( '$html .=' ), array( 'echo' ), $source );
+
+	$js_html = "";
+	if (file_exists (DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js")) {
+		$js_source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js" );
+		$js_html = "
+		<h2>vulnerabilities/{$id}/source/{$security}.js</h2>
+		<div id=\"code\">
+			<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+				<tr>
+					<td><div id=\"code\">" . highlight_string( $js_source, true ) . "</div></td>
+				</tr>
+			</table>
+		</div>
+		";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln} Source</h1>
+
+		<h2>vulnerabilities/{$id}/source/{$security}.php</h2>
+		<div id=\"code\">
+			<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+				<tr>
+					<td><div id=\"code\">" . highlight_string( $source, true ) . "</div></td>
+				</tr>
+			</table>
+		</div>
+		{$js_html}
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"Compare All Levels\" onclick=\"window.location.href='view_source_all.php?id=$id'\">
+		</form>
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
 }
-
-$source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.php" );
-$source = str_replace( array( '$html .=' ), array( 'echo' ), $source );
-
-$js_html = "";
-if (file_exists (DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js")) {
-	$js_source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js" );
-	$js_html = "
-	<h2>vulnerabilities/{$id}/source/{$security}.js</h2>
-	<div id=\"code\">
-		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-			<tr>
-				<td><div id=\"code\">" . highlight_string( $js_source, true ) . "</div></td>
-			</tr>
-		</table>
-	</div>
-	";
-}
-
-$page[ 'body' ] .= "
-<div class=\"body_padded\">
-	<h1>{$vuln} Source</h1>
-
-	<h2>vulnerabilities/{$id}/source/{$security}.php</h2>
-	<div id=\"code\">
-		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-			<tr>
-				<td><div id=\"code\">" . highlight_string( $source, true ) . "</div></td>
-			</tr>
-		</table>
-	</div>
-	{$js_html}
-	<br /> <br />
-
-	<form>
-		<input type=\"button\" value=\"Compare All Levels\" onclick=\"window.location.href='view_source_all.php?id=$id'\">
-	</form>
-</div>\n";
 
 dvwaSourceHtmlEcho( $page );
 
--- vulnerabilities/view_source_all.php
+++ vulnerabilities/view_source_all.php
@@ -8,104 +8,108 @@ dvwaPageStartup( array( 'authenticated', 'phpids' ) );
 $page = dvwaPageNewGrab();
 $page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
 
-$id = $_GET[ 'id' ];
+if (array_key_exists ("id", $_GET)) {
+	$id = $_GET[ 'id' ];
 
-$lowsrc = @file_get_contents("./{$id}/source/low.php");
-$lowsrc = str_replace( array( '$html .=' ), array( 'echo' ), $lowsrc);
-$lowsrc = highlight_string( $lowsrc, true );
+	$lowsrc = @file_get_contents("./{$id}/source/low.php");
+	$lowsrc = str_replace( array( '$html .=' ), array( 'echo' ), $lowsrc);
+	$lowsrc = highlight_string( $lowsrc, true );
 
-$medsrc = @file_get_contents("./{$id}/source/medium.php");
-$medsrc = str_replace( array( '$html .=' ), array( 'echo' ), $medsrc);
-$medsrc = highlight_string( $medsrc, true );
+	$medsrc = @file_get_contents("./{$id}/source/medium.php");
+	$medsrc = str_replace( array( '$html .=' ), array( 'echo' ), $medsrc);
+	$medsrc = highlight_string( $medsrc, true );
 
-$highsrc = @file_get_contents("./{$id}/source/high.php");
-$highsrc = str_replace( array( '$html .=' ), array( 'echo' ), $highsrc);
-$highsrc = highlight_string( $highsrc, true );
+	$highsrc = @file_get_contents("./{$id}/source/high.php");
+	$highsrc = str_replace( array( '$html .=' ), array( 'echo' ), $highsrc);
+	$highsrc = highlight_string( $highsrc, true );
 
-$impsrc = @file_get_contents("./{$id}/source/impossible.php");
-$impsrc = str_replace( array( '$html .=' ), array( 'echo' ), $impsrc);
-$impsrc = highlight_string( $impsrc, true );
+	$impsrc = @file_get_contents("./{$id}/source/impossible.php");
+	$impsrc = str_replace( array( '$html .=' ), array( 'echo' ), $impsrc);
+	$impsrc = highlight_string( $impsrc, true );
 
-switch ($id) {
-	case "javascript" :
-		$vuln = 'JavaScript';
-		break;
-	case "fi" :
-		$vuln = 'File Inclusion';
-		break;
-	case "brute" :
-		$vuln = 'Brute Force';
-		break;
-	case "csrf" :
-		$vuln = 'CSRF';
-		break;
-	case "exec" :
-		$vuln = 'Command Injection';
-		break;
-	case "sqli" :
-		$vuln = 'SQL Injection';
-		break;
-	case "sqli_blind" :
-		$vuln = 'SQL Injection (Blind)';
-		break;
-	case "upload" :
-		$vuln = 'File Upload';
-		break;
-	case "xss_r" :
-		$vuln = 'Reflected XSS';
-		break;
-	case "xss_s" :
-		$vuln = 'Stored XSS';
-		break;
-	case "weak_id" :
-		$vuln = 'Weak Session IDs';
-		break;
-	default:
-		$vuln = "Unknown Vulnerability";
+	switch ($id) {
+		case "javascript" :
+			$vuln = 'JavaScript';
+			break;
+		case "fi" :
+			$vuln = 'File Inclusion';
+			break;
+		case "brute" :
+			$vuln = 'Brute Force';
+			break;
+		case "csrf" :
+			$vuln = 'CSRF';
+			break;
+		case "exec" :
+			$vuln = 'Command Injection';
+			break;
+		case "sqli" :
+			$vuln = 'SQL Injection';
+			break;
+		case "sqli_blind" :
+			$vuln = 'SQL Injection (Blind)';
+			break;
+		case "upload" :
+			$vuln = 'File Upload';
+			break;
+		case "xss_r" :
+			$vuln = 'Reflected XSS';
+			break;
+		case "xss_s" :
+			$vuln = 'Stored XSS';
+			break;
+		case "weak_id" :
+			$vuln = 'Weak Session IDs';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
 }
-
-$page[ 'body' ] .= "
-<div class=\"body_padded\">
-	<h1>{$vuln}</h1>
-	<br />
-
-	<h3>Impossible {$vuln} Source</h3>
-	<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-		<tr>
-			<td><div id=\"code\">{$impsrc}</div></td>
-		</tr>
-	</table>
-	<br />
-
-	<h3>High {$vuln} Source</h3>
-	<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-		<tr>
-			<td><div id=\"code\">{$highsrc}</div></td>
-		</tr>
-	</table>
-	<br />
-
-	<h3>Medium {$vuln} Source</h3>
-	<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-		<tr>
-			<td><div id=\"code\">{$medsrc}</div></td>
-		</tr>
-	</table>
-	<br />
-
-	<h3>Low {$vuln} Source</h3>
-	<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-		<tr>
-			<td><div id=\"code\">{$lowsrc}</div></td>
-		</tr>
-	</table>
-	<br /> <br />
-
-	<form>
-		<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
-	</form>
-
-</div>\n";
 
 dvwaSourceHtmlEcho( $page );
 
