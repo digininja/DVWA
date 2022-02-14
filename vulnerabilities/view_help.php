155210012c60186502ecb9c435060ef2942d8e72
Robin Wood <robin@digi.ninja>
2022-02-14 21:20:50 +0000

    check parameters exist before accessing

--- vulnerabilities/view_help.php
+++ vulnerabilities/view_help.php
@@ -8,18 +8,24 @@ dvwaPageStartup( array( 'authenticated', 'phpids' ) );
 $page = dvwaPageNewGrab();
 $page[ 'title' ] = 'Help' . $page[ 'title_separator' ].$page[ 'title' ];
 
-$id       = $_GET[ 'id' ];
-$security = $_GET[ 'security' ];
-$locale = $_GET[ 'locale' ];
+if (array_key_exists ("id", $_GET) &&
+	array_key_exists ("security", $_GET) &&
+	array_key_exists ("locale", $_GET)) {
+	$id       = $_GET[ 'id' ];
+	$security = $_GET[ 'security' ];
+	$locale = $_GET[ 'locale' ];
 
-ob_start();
-if ($locale == 'en') {
-	eval( '?>' . file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php" ) . '<?php ' );
+	ob_start();
+	if ($locale == 'en') {
+		eval( '?>' . file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php" ) . '<?php ' );
+	} else {
+		eval( '?>' . file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php" ) . '<?php ' );
+	}
+	$help = ob_get_contents();
+	ob_end_clean();
 } else {
-	eval( '?>' . file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php" ) . '<?php ' );
+	$help = "<p>Not Found</p>";
 }
-$help = ob_get_contents();
-ob_end_clean();
 
 $page[ 'body' ] .= "
 <div class=\"body_padded\">
