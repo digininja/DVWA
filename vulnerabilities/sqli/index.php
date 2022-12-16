4d7bc01d5a3245c8f9dd9ec838bada9ec5f43fec
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2022-12-16 11:41:45 +0100

    Clean error

--- vulnerabilities/sqli/index.php
+++ vulnerabilities/sqli/index.php
@@ -1,5 +1,5 @@
 <?php
-
+// TODO
 define( 'DVWA_WEB_PAGE_TO_ROOT', '../../' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -2,13 +2,14 @@
 
 if( isset( $_REQUEST[ 'Submit' ] ) ) {
 	// Get input
-	$id = $_REQUEST[ 'id' ];
+	// $id = $_REQUEST[ 'id' ];
+	$id = "ID";
 
 	switch ($_DVWA['SQLI_DB']) {
 		case MYSQL:
 			// Check database
 			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query );
 
 			// Get results
 			while( $row = mysqli_fetch_assoc( $result ) ) {
