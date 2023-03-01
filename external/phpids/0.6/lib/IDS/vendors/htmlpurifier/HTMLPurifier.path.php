c47b6ba73119afd7b85860564097e06822a7536b
Robin Wood <robin@digi.ninja>
2023-03-01 13:03:59 +0000

    removed PHP IDS library

--- external/phpids/0.6/lib/IDS/vendors/htmlpurifier/HTMLPurifier.path.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @file
- * Convenience stub file that adds HTML Purifier's library file to the path
- * without any other side-effects.
- */
-
-set_include_path(dirname(__FILE__) . PATH_SEPARATOR . get_include_path() );
