0e3c155512f824799b22ebd275514565e4d2b255
Robin Wood <robin@digi.ninja>
2023-12-10 16:01:26 +0000

    Updated CSP Bypass Low
    Changed the exercise a little bit to have the user try to understand why
    Pastebin and its friends stopped working.

--- vulnerabilities/csp/source/low.php
+++ vulnerabilities/csp/source/low.php
@@ -1,6 +1,6 @@
 <?php
 
-$headerCSP = "Content-Security-Policy: script-src 'self' https://pastebin.com hastebin.com www.toptal.com example.com code.jquery.com https://ssl.google-analytics.com ;"; // allows js from self, pastebin.com, hastebin.com, jquery and google analytics.
+$headerCSP = "Content-Security-Policy: script-src 'self' https://pastebin.com hastebin.com www.toptal.com example.com code.jquery.com https://ssl.google-analytics.com https://digi.ninja ;"; // allows js from self, pastebin.com, hastebin.com, jquery, digi.ninja, and google analytics.
 
 header($headerCSP);
 
@@ -21,4 +21,17 @@ $page[ 'body' ] .= '
 	<input size="50" type="text" name="include" value="" id="include" />
 	<input type="submit" value="Include" />
 </form>
+<p>
+	As Pastebin and Hastebin have stopped working, here are some scripts that may, or may not help.
+</p>
+<ul>
+	<li>https://digi.ninja/dvwa/alert.js</li>
+	<li>https://digi.ninja/dvwa/alert.txt</li>
+	<li>https://digi.ninja/dvwa/cookie.js</li>
+	<li>https://digi.ninja/dvwa/forced_download.js</li>
+	<li>https://digi.ninja/dvwa/wrong_content_type.js</li>
+</ul>
+<p>
+	Pretend these are on a server like Pastebin and try to work out why some work and some do not work. Check the help for an explanation if you get stuck.
+</p>
 ';
