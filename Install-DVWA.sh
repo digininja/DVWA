eabbd608685474d2d3094a03087179333a28cbf4
IamCarron <47905563+IamCarron@users.noreply.github.com>
2024-01-16 17:45:16 +0100

    Update Install-DVWA.sh

--- Install-DVWA.sh
+++ Install-DVWA.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Obtener el prefijo del idioma
+lang_prefix="${LANG:0:2}"
+
 # Función para verificar el idioma y mostrar el mensaje correspondiente
 get_language_message() {
-    if [[ $LANG == *"es"* ]]; then
+    if [[ $lang_prefix == "es" ]]; then
         echo -e "$1"
     else
         echo -e "$2"
@@ -18,16 +21,20 @@ fi
 
 # Función para verificar la existencia de un programa
 check_program() {
-    command -v "$1" >/dev/null 2>&1 || {
+    if ! command -v "$1" &>/dev/null; then
         message=$(get_language_message "\033[91m$1 no está instalado. Instalándolo ahora..." "\033[91m$1 is not installed. Installing it now...")
         echo -e >&2 "$message"
         apt install -y "$1"
-    }
+    else
+        success_message=$(get_language_message "\033[92m$1 está instalado.\033[0m" "\033[92m$1 is installed.\033[0m")
+        echo -e "$success_message"
+    fi
 }
+
 # Función para prompt de contraseña de MySQL root
 get_mysql_root_password() {
     read -s -p "$(get_language_message "Enter MySQL root password: " "Ingrese la contraseña de root de MySQL: ")" mysql_root_password
-    echo "$mysql_root_password"
+    echo -e "$mysql_root_password"
 }
 # Arte ASCII
 echo -e "\033[96m\033[1m
@@ -52,7 +59,6 @@ echo -e "\033[96m\033[1m
 welcome_message=$(get_language_message "\033[96mWelcome to the DVWA installer!\033[0m" "\033[96mBienvenido al instalador de DVWA!\033[0m")
 echo -e "$welcome_message"
 
-echo
 echo
 # Inicio del Script
 update_message=$(get_language_message "\e[96mUpdating repositories...\e[0m" "\e[96mActualizando repositorios...\e[0m")
@@ -89,7 +95,7 @@ mysql -u root -p$mysql_root_password -e "CREATE DATABASE IF NOT EXISTS dvwa;"
 mysql -u root -p$mysql_root_password -e "CREATE USER 'dvwa'@'localhost' IDENTIFIED BY 'abc123';"
 mysql -u root -p$mysql_root_password -e "GRANT ALL PRIVILEGES ON dvwa.* TO 'dvwa'@'localhost';"
 mysql -u root -p$mysql_root_password -e "FLUSH PRIVILEGES;"
-
+echo
 # Mensaje de éxito
 success_message=$(get_language_message "\e[92mMySQL commands executed successfully.\e[0m" "\e[92mComandos MySQL ejecutados correctamente.\e[0m")
 echo -e "$success_message"
@@ -110,11 +116,30 @@ sleep 2
 
 php_config_message=$(get_language_message "\e[96mConfiguring PHP...\e[0m" "\e[96mConfigurando PHP...\e[0m")
 echo -e "$php_config_message"
-php_config_file="/etc/php/$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')/apache2/php.ini"
-sed -i 's/^\(allow_url_include =\).*/\1 on/' $php_config_file
-sed -i 's/^\(allow_url_fopen =\).*/\1 on/' $php_config_file
-sed -i 's/^\(display_errors =\).*/\1 on/' $php_config_file
-sed -i 's/^\(display_startup_errors =\).*/\1 on/' $php_config_file
+# Intentar encontrar el archivo php.ini en la carpeta de Apache
+php_config_file_apache="/etc/php/$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')/apache2/php.ini"
+
+# Intentar encontrar el archivo php.ini en la carpeta de FPM
+php_config_file_fpm="/etc/php/$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')/fpm/php.ini"
+
+# Comprobar si existe el archivo php.ini en la carpeta de Apache y usarlo si está presente
+if [ -f "$php_config_file_apache" ]; then
+    php_config_file="$php_config_file_apache"
+    sed -i 's/^\(allow_url_include =\).*/\1 on/' $php_config_file
+    sed -i 's/^\(allow_url_fopen =\).*/\1 on/' $php_config_file
+    sed -i 's/^\(display_errors =\).*/\1 on/' $php_config_file
+    sed -i 's/^\(display_startup_errors =\).*/\1 on/' $php_config_file
+# Comprobar si existe el archivo php.ini en la carpeta de FPM y usarlo si está presente
+elif [ -f "$php_config_file_fpm" ]; then
+    php_config_file="$php_config_file_fpm"
+    sed -i 's/^\(allow_url_include =\).*/\1 on/' $php_config_file
+    sed -i 's/^\(allow_url_fopen =\).*/\1 on/' $php_config_file
+    sed -i 's/^\(display_errors =\).*/\1 on/' $php_config_file
+    sed -i 's/^\(display_startup_errors =\).*/\1 on/' $php_config_file
+else
+    # Mensaje de advertencia si no se encuentra en ninguna de las carpetas
+    echo -e "\e[91mWarning: PHP configuration file not found in Apache or FPM folders.\e[0m"
+fi
 sleep 2
 
 apache_restart_message=$(get_language_message "\e[96mRestarting Apache...\e[0m" "\e[96mReiniciando Apache...\e[0m")
@@ -135,6 +160,5 @@ echo -e "$credentials_after_setup_message"
 echo -e "Username: \033[93madmin\033[0m"
 echo -e "Password: \033[93mpassword\033[0m"
 
-echo
 echo
 echo -e "\033[91mCon ♡ by Iamcarron"
