077896a4f5d895ba82999bb21ea44adebedd5ba2
IamCarron <47905563+IamCarron@users.noreply.github.com>
2024-01-17 10:51:16 +0100

    Update Install-DVWA.sh

--- Install-DVWA.sh
+++ Install-DVWA.sh
@@ -166,7 +166,7 @@ success_message=$(get_language_message "\e[92mDVWA has been installed successful
 echo -e "$success_message"
 
 #Show to user the credentials after setup
-credentials_after_setup_message=$(get_language_message "\e[92mCredentials after setup:\e[0m" "\e[92mCredenciales después de la configuración:\e[0m")
+credentials_after_setup_message=$(get_language_message "\e[92mCredentials:\e[0m" "\e[92mCredenciales:\e[0m")
 echo -e "$credentials_after_setup_message"
 echo -e "Username: \033[93madmin\033[0m"
 echo -e "Password: \033[93mpassword\033[0m"
