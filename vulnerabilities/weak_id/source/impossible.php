aee770c33fbdc6c2f06b5646c54bc18a166e2132
Mikhail Fedosov <mikhail.fedosov@vk.team>
2024-05-11 12:09:28 +0300

    bug: cookie is not set if the host address is localhost:4320

--- vulnerabilities/weak_id/source/impossible.php
+++ vulnerabilities/weak_id/source/impossible.php
@@ -4,6 +4,6 @@ $html = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$cookie_value = sha1(mt_rand() . time() . "Impossible");
-	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], true, true);
+	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['SERVER_NAME'], true, true);
 }
 ?>
