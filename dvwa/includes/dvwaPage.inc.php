c07c4b2133e9fe6dc732d521d9fc9863fc69f098
Robin Wood <robin@digi.ninja>
2024-09-04 22:37:14 +0100

    don't title if title is empty

--- dvwa/includes/dvwaPage.inc.php
+++ dvwa/includes/dvwaPage.inc.php
@@ -513,7 +513,7 @@ function dvwaSourceHtmlEcho( $pPage ) {
 
 // To be used on all external links --
 function dvwaExternalLinkUrlGet( $pLink,$text=null ) {
-	if(is_null( $text )) {
+	if(is_null( $text ) || $text == "") {
 		return '<a href="' . $pLink . '" target="_blank">' . $pLink . '</a>';
 	}
 	else {
