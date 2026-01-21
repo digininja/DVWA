df6c04c910554a8bee2fd40937cafda396863eae
Robin Wood <robin@digi.ninja>
2026-01-21 11:56:36 +0000

    working CSP bypass domains

--- vulnerabilities/csp/help/help.php
+++ vulnerabilities/csp/help/help.php
@@ -18,13 +18,17 @@
 
 		<h3>Low Level</h3>
 		<p>Examine the policy to find all the sources that can be used to host external script files.</p>
-		<p>This exercise was originally written to work with Pastebin, then updated for Hastebin, then Toptal, but all these stopped working as they set various headers that prevent the browser executing the JavaScript once it has downloaded it. To get around this, there are a selection of links included in the exercise, some will work, some will not, try to work out why.
+		<p>This exercise was originally written to work with Pastebin, then updated for Hastebin, then Toptal, but all these stopped working as they set various headers that prevent the browser executing the JavaScript once it has downloaded it. Since then two new services have been identified, UNPKG and JSDELIVR, the first is a proxy for NPM packages, the second one for GitHub files. They are both designed to allow raw access to any files and do not set any headers that will stop injection.
+		</p>
+		<p>I have also put a number of files on my site which help to demonstrate how different headers and file extensions can block execution.</p>
 		<pre>Spoiler: <span class="spoiler">
-alert.js - Will work, this is a normal JavaScript file served with the correct headers.
-alert.txt - This will not work as it has the wrong content type set by the web server due to its file extension.
-cookie.js - This will work and will show your cookies
-forced_download.js - As the name says, the server sets the "Content-Disposition: attachment" header for this to force the browser to download it rather than execute it.
-wrong_content_type.js - This will not work as the web server ignores the file extension and forces the content type to get set as "plain/text" which prevents the browser executing it.</span></pre>
+https://cdn.jsdelivr.net/gh/digininja/csp_bypass/alert.js - Using JSDELIVR to server a JavaScript file stored on GitHub.
+https://unpkg.com/@digininja/csp_bypass@1.0.0/index.js - Using UNPKG to access a JavaScript file in an NPM package.
+https://digi.ninja/dvwa/alert.js - Will work, this is a normal JavaScript file served with the correct headers.
+https://digi.ninja/dvwa/alert.txt - This will not work as it has the wrong content type set by the web server due to its file extension.
+https://digi.ninja/dvwa/cookie.js - This will work and will show your cookies
+https://digi.ninja/dvwa/forced_download.js - As the name says, the server sets the "Content-Disposition: attachment" header for this to force the browser to download it rather than execute it.
+https://digi.ninja/dvwa/wrong_content_type.js - This will not work as the web server ignores the file extension and forces the content type to get set as "plain/text" which prevents the browser executing it.</span></pre>
 
 		<br />
 
--- vulnerabilities/csp/source/low.php
+++ vulnerabilities/csp/source/low.php
@@ -1,12 +1,12 @@
 <?php
 
-$headerCSP = "Content-Security-Policy: script-src 'self' https://pastebin.com hastebin.com www.toptal.com example.com code.jquery.com https://ssl.google-analytics.com https://digi.ninja ;"; // allows js from self, pastebin.com, hastebin.com, jquery, digi.ninja, and google analytics.
+$headerCSP = "Content-Security-Policy: script-src 'self' https://pastebin.com hastebin.com www.toptal.com example.com code.jquery.com https://ssl.google-analytics.com unpkg.com cdn.jsdelivr.net digi.ninja ;"; // allows js from various trusted locations
 
 header($headerCSP);
 
 # These might work if you can't create your own for some reason
-# https://pastebin.com/raw/R570EE00
-# https://www.toptal.com/developers/hastebin/raw/cezaruzeka
+# https://cdn.jsdelivr.net/gh/digininja/csp_bypass/alert.js>
+# https://unpkg.com/@digininja/csp_bypass@1.0.0/index.js
 
 ?>
 <?php
@@ -22,16 +22,6 @@ $page[ 'body' ] .= '
 	<input type="submit" value="Include" />
 </form>
 <p>
-	As Pastebin and Hastebin have stopped working, here are some scripts that may, or may not help.
-</p>
-<ul>
-	<li>https://digi.ninja/dvwa/alert.js</li>
-	<li>https://digi.ninja/dvwa/alert.txt</li>
-	<li>https://digi.ninja/dvwa/cookie.js</li>
-	<li>https://digi.ninja/dvwa/forced_download.js</li>
-	<li>https://digi.ninja/dvwa/wrong_content_type.js</li>
-</ul>
-<p>
-	Pretend these are on a server like Pastebin and try to work out why some work and some do not work. Check the help for an explanation if you get stuck.
+	You will probably need to do some reading up on what some of the domains allowed by the CSP do and how they can be used.
 </p>
 ';
