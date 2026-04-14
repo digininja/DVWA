934fe2198852af91b64de2f8a16e53b9b34f9d36
Sumedh R Mundewadi <152703806+Sumedh-6504@users.noreply.github.com>
2026-04-14 09:54:06 +0530

    🔒 Automated Security Patch for vulnerabilities/javascript/source/medium.js

--- vulnerabilities/javascript/source/medium.js
+++ vulnerabilities/javascript/source/medium.js
@@ -1,1 +1,12 @@
-function do_something(e){for(var t="",n=e.length-1;n>=0;n--)t+=e[n];return t}setTimeout(function(){do_elsesomething("XX")},300);function do_elsesomething(e){document.getElementById("token").value=do_something(e+document.getElementById("phrase").value+"XX")}
+function do_something(e){for(var t="",n=e.length-1;n>=0;n--)t+=e[n];return t}
+setTimeout(function(){do_elsesomething("XX")},300);
+function do_elsesomething(e){
+    var phrase = document.getElementById("phrase").value;
+    var sanitizedPhrase = phrase
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+    document.getElementById("token").value = do_something(e + sanitizedPhrase + "XX")
+}
